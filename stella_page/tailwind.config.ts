ef84e2927d99dd472c4d4e674141ea35be8503a5
Harrison <hproloff@gmail.com>
2024-01-18 13:47:16 -0600

    changed final files

--- stella_page/tailwind.config.ts
+++ stella_page/tailwind.config.ts
@@ -1,20 +1,25 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+module.exports = {
+  mode: "jit",
+  purge: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        custom: {
+          lightGray: "#F6F8FB",
+          dark: "#5C637C",
+        },
       },
     },
   },
-  plugins: [],
-}
-export default config
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/line-clamp"),
+    require("tailwind-scrollbar-hide"),
+  ],
+};
